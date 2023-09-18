157d146cb871f9679177c2b59f27c0c49e16c2a0
yolovi <yolandalopevi@gmail.com>
2023-09-18 17:03:55 +0200

    erased events content

--- src/components/Events/CreateEvent/CreateEvent.jsx
+++ src/components/Events/CreateEvent/CreateEvent.jsx
@@ -1,44 +1,9 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { createEvent } from "../../../features/events/eventsSlice";
+import React from 'react'
 
 const CreateEvent = () => {
-  const [formData, setFormData] = useState({
-    /* MODIFICAR */
-    doubt: "",
-    topic: "",
-    question: "",
-  });
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const handleInputChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(createEvent(formData));
-    /* ¿ES NECESARIO? */
-    setTimeout(() => {
-      navigate("/");
-    }, 1000);
-  };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nombre del evento"
-          /* MIRAR EN EL BACK */
-          name="eventName"
-          onChange={handleInputChange}
-        />
-      </form>
-    </>
-  );
-};
+    <div>CreateEvent</div>
+  )
+}
 
-export default CreateEvent;
+export default CreateEvent
