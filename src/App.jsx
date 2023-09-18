214c0aeee25fafbd81eaa2869ed0d935dfca0293
yolovi <yolandalopevi@gmail.com>
2023-09-18 11:48:38 +0200

    envent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Register from "./components/User/Register/Register";
 import Login from "./components/User/Login/Login";
+import Event from "./components/Events/Event";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/event" element={<Event />} />
           </Route>
         </Routes>
       </BrowserRouter>
