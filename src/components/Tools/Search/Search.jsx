514d1ee374307fe6acfe1e94edd0fedca701c2a9
yolovi <yolandalopevi@gmail.com>
2023-09-24 18:52:40 +0200

    search component created not working

--- src/components/Tools/Search/Search.jsx
+++ src/components/Tools/Search/Search.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getEventsByName } from "../../features/Events/eventsSlice";
-import GetEvents from "../Events/GetEvents";
+// import React, { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import { getEventsByName } from "../../features/Events/eventsSlice";
+// import GetEvents from "../Events/GetEvents";
 
-const Search = () => {
-  const { title } = useParams();
-  const dispatch = useDispatch();
+// const Search = () => {
+//   const { title } = useParams();
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getEventsByName(title));
-  }, [title]);
+//   useEffect(() => {
+//     dispatch(getEventsByName(title));
+//   }, [title]);
   
-  if (title == null) {
-    return <div>{<GetEvents />}</div>;
-  }
+//   if (title == null) {
+//     return <div>{<GetEvents />}</div>;
+//   }
 
 
-  return <div>{<GetEvents />}</div>;
-};
+//   return <div>{<GetEvents />}</div>;
+// };
 
-export default Search;
+// export default Search;
