0a6aa873dc480445dd26a63835bb9a455e346f4d
JuandaMT <juandamt@outlook.es>
2023-09-24 17:56:27 +0200

    corregido comportamiento scroll

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,13 +7,16 @@ const Home = () => {
     <>
       <h2>ESTÁS EN HOME</h2>
       <h2>SearchBar</h2>
-      <h2>Filtros</h2>
-      <h2>Para ti</h2>
+      <div className="filtros-home">
+        <h2>Filtros</h2>
+        
+      </div>
       <section className="paraTi">
+        <h2>Para ti</h2>
         <CarrouselEvents />
-        <h2>Próximos eventos</h2>
       </section>
-      <section>
+      <section className="proximos-eventos">
+        <h2 className="titulo">Próximos eventos</h2>
         <GetEvents />
       </section>
     </>
