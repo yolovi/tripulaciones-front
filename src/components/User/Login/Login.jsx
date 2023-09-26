5c304a6612c32b271b3e2b81e74c779e185f9520
pafz <paferza@gmail.com>
2023-09-26 12:13:54 +0200

    Login view WIP

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login, reset } from "../../../features/auth/authSlice";
@@ -9,24 +10,41 @@ import {
   AlertDescription,
 } from "@chakra-ui/react";
 import "./Login.scss";
+=======
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, reset } from '../../../features/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
+// import {
+//   Alert,
+//   AlertIcon,
+//   AlertTitle,
+//   AlertDescription,
+// } from "@chakra-ui/react";
+import './Login.scss';
+>>>>>>> Stashed changes
 // import petsSperience from "../../assets/images/Petspierince1.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faAt,
   faCamera,
   faFaceSmile,
   faLock,
   faMobile,
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const { email, password } = formData;
+<<<<<<< Updated upstream
  const { isSuccess, isError, message } = useSelector((state) => state.auth);
+=======
+  // const { isSuccess, isError, message } = useSelector((state) => state.auth);
+>>>>>>> Stashed changes
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -47,13 +65,13 @@ const Login = () => {
     }
   }, [isSuccess, isError, message, navigate, dispatch]);
 
-  const onChange = (e) =>
-    setFormData((prevState) => ({
+  const onChange = e =>
+    setFormData(prevState => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
 
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     e.preventDefault();
     dispatch(login(formData));
   };
@@ -82,45 +100,41 @@ const Login = () => {
         <div className="card-login">
           <div className="form-data">
             <form className="form" onSubmit={onSubmit}>
-              <p id="heading">Login</p>
+              <p id="heading">Correo electrónico*</p>
               <div className="field">
-                <FontAwesomeIcon icon={faAt} style={{ color: "#3c5b90" }} />
+                <FontAwesomeIcon icon={faAt} style={{ color: '#3c5b90' }} />
                 <input
                   className="input-field"
                   type="email"
                   name="email"
                   value={email}
-                  placeholder="email"
+                  placeholder="EJ: pepito@pepito.com"
                   onChange={onChange}
                   required
                 />
               </div>
+              <p id="heading">Contraseña*</p>
               <div className="field">
-                <FontAwesomeIcon icon={faLock} style={{ color: "#3c5b90" }} />
+                <p></p>
+                <FontAwesomeIcon icon={faLock} style={{ color: '#3c5b90' }} />
                 <input
                   className="input-field"
                   type="password"
                   name="password"
                   value={password}
-                  placeholder="password"
+                  placeholder="Indica tu contraseña"
                   onChange={onChange}
                   required
                 />
               </div>
+              <a href="./">¿Olvidaste la contraseña?</a>
               <div className="container-btn">
                 <button className="button1" type="submit">
-                  Login
+                  Acceder
                 </button>
-                <button
-                  className="button2"
-                  onClick={() => {
-                    setTimeout(() => {
-                      navigate("/register");
-                    }, 200);
-                  }}
-                >
-                  Sign Up
-                </button>
+                <a href="./register" className="link-register">
+                  No tengo cuenta
+                </a>
               </div>
             </form>
           </div>
